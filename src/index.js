664d4d83f7069d6f85ac259453c0922e371a5739
Mario <albeto8@gmail.com>
2018-08-16 12:50:09 -0500

    Youtube search response

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
 //import reducers from './reducers';
 
 const API_KEY = 'AIzaSyDRE9L00l3eGyrZUld2sgnc_rj_bcz6WpA';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return <div>
