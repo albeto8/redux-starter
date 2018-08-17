4b9cfe034b317a64d0dd0ef5da4d420130a1aff4
Mario <>
2018-08-17 17:20:07 -0500

    styling CSS

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,12 +8,11 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term}
           onChange={event => this.setState({ term: event.target.value }) }
         />
-        Value input: {this.state.term}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-import VideoDetail from './components/video_details';
+import VideoDetail from './components/video_detail';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
 //import reducers from './reducers';
