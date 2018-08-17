6bacde654ffbcf211180e85b076da971e9f6b58c
Mario <>
2018-08-17 17:13:50 -0500

    Video selection

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 //import { Provider } from 'react-redux';
 //import { createStore, applyMiddleware } from 'redux';
 //import reducers from './reducers';
 
 const API_KEY = 'AIzaSyDRE9L00l3eGyrZUld2sgnc_rj_bcz6WpA';
 
-YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
-  console.log(data);
-});
 
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
